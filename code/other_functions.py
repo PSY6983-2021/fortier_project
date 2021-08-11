419e1db1995bd845a2e5b98a41d97d3ff9daa57c
efortier <eddy.fortier@umontreal.ca>
2021-08-11 15:31:53 -0400

    creation of the make_sub_df fct in other_functions.py

--- code/other_functions.py
+++ code/other_functions.py
@@ -33,15 +33,32 @@ def eliminate_row(df, column_to_search, value_to_search):
     #return df.drop(to_drop, axis = 0)
     return df
 
-def plot_graph(df, test_type, ear = "Bilateral"):
+def make_sub_df(df, columns, row, test_type):
+    pta_x = [250, 500, 1000, 2000,
+             3000, 4000, 6000, 8000,
+             9000, 10000, 11200, 12500,
+             14000, 16000, 18000, 20000]
+    mtx_x = ["Noise: Left, Speech: Left",
+             "Noise: Binaural, Speech: Left",
+             "Noise: Binaural, Speech: Binaural",
+             "Noise: Binaural, Speech: Right",
+             "Noise: Right, Speech: Right"]
+    print(pta_x)
+    print(mtx_x)
+    if test_type == "pta":
+        x = pta_x
+    elif test_type == "mtx":
+        x = mtx_x
+
+
+def plot_pta_R(df):
     """
     INPUTS
     -df: pandas dataframe containing the data to plot
-    -test_type: takes a str value that can either be \"pta\" or \"mtx\"
-    -ear: takes a str with the ear side to plot ([Right, Left, Bilateral], default is Bilateral)
+#   -test_type: takes a str value that can either be \"pta\" or \"mtx\"
+#   -ear: takes a str with the ear side to plot ([Right, Left, Bilateral], default is Bilateral)
     OUTPUTS
     """
-    counter = 0
 
     if test_type == "pta":
         if ear == "Right":
@@ -49,15 +66,21 @@ def plot_graph(df, test_type, ear = "Bilateral"):
                          "RE_3000", "RE_4000", "RE_6000", "RE_8000",
                          "RE_9000", "RE_10000", "RE_11200", "RE_12500",
                          "RE_14000", "RE_16000", "RE_18000", "RE_20000"]
-            x_labels = {"RE_250": 250, "RE_500": 500, "RE_1000": 1000, "RE_2000": 2000,
-                        "RE_3000": 3000, "RE_4000": 4000, "RE_6000": 6000, "RE_8000": 8000,
-                        "RE_9000": 9000, "RE_10000": 10000, "RE_11200": 11200, "RE_12500": 12500,
-                        "RE_14000": 14000, "RE_16000": 16000, "RE_18000": 18000, "RE_20000": 20000}
+#           x_labels = {"RE_250": 250, "RE_500": 500, "RE_1000": 1000, "RE_2000": 2000,
+#                       "RE_3000": 3000, "RE_4000": 4000, "RE_6000": 6000, "RE_8000": 8000,
+#                       "RE_9000": 9000, "RE_10000": 10000, "RE_11200": 11200, "RE_12500": 12500,
+#                       "RE_14000": 14000, "RE_16000": 16000, "RE_18000": 18000, "RE_20000": 20000}
+            sub_df_R = make_sub_df(df, x_columns, counter, "pta")
+            sub_df_L = []
+
         elif ear == "Left":
             x_columns = ["LE_250", "LE_500", "LE_1000", "LE_2000",
                          "LE_3000", "LE_4000", "LE_6000", "LE_8000",
                          "LE_9000", "LE_10000", "LE_11200", "LE_12500",
                          "LE_14000", "LE_16000", "LE_18000", "LE_20000"]
+            sub_df_R = []
+            sub_df_L = make_sub_df(df, x_columns, counter, "pta")
+
         elif ear == "Bilateral":
             x1_columns = ["RE_250", "RE_500", "RE_1000", "RE_2000",
                           "RE_3000", "RE_4000", "RE_6000", "RE_8000",
@@ -67,6 +90,8 @@ def plot_graph(df, test_type, ear = "Bilateral"):
                           "LE_3000", "LE_4000", "LE_6000", "LE_8000",
                           "LE_9000", "LE_10000", "LE_11200", "LE_12500",
                           "LE_14000", "LE_16000", "LE_18000", "LE_20000"]
+            sub_df_R = make_sub_df(df, x1_columns, counter, "pta")
+            sub_df_L = make_sub_df(df, x2_columns, counter, "pta")
         else:
             print("ERROR: the test_type value isn't valid")
 
@@ -107,15 +132,9 @@ def plot_graph(df, test_type, ear = "Bilateral"):
         fig = px.line(data, title = title, log_x = True, range_x = [100, 20000], range_y = [-20, 80])
         fig.show()
         fig.write_image("../results/" + title + ".png")
-        counter =+ 1
         #pass
     elif test_type == "mtx":
         pass
     else:
         print("The test_type value is not valid. The only valid values are str containing the value \"pta\" or the value \"mtx\".")
         pass
-
-    if counter <= 1:
-        print(counter, ".png file was saved.")
-    else:
-        print(counter, ".png files were saved.")
